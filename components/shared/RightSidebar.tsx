e7adb8504676936dd6ad2115e95060fc4adbe7f4
Ol1eg <olegzarickij1@gmail.com>
2023-11-15 17:59:17 +0200

    Create Home, OnBoarding pages

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,18 @@
+function RightSidebar(){
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">
+                    Suggested Agency
+                </h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">
+                    Suggested advertiser
+                </h3>
+            </div>
+        </section>
+    )
+}
+
+export default RightSidebar
